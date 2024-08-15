104cfe8f960d0f396e6d1fd93ee0865f18c0a6c8
Chinmay-innovates <chinmaygope69@gmail.com>
2024-08-15 22:17:43 +0530

    add show_hover( )

--- src/game.py
+++ src/game.py
@@ -7,6 +7,7 @@ from dragger import Dragger
 class Game:
     def __init__(self):
         self.next_player = 'white'
+        self.hovered_square = None
         self.board = Board()
         self.dragger =Dragger()
     #show methords
@@ -57,6 +58,19 @@ class Game:
              #blit
              pygame.draw.rect(surface, color, rect)
     
+    def show_hover(self,surface):
+        if self.hovered_square:
+            #color
+            color =(180,180,180) 
+            #rect
+            rect =(self.hovered_square.col*SQSIZE,self.hovered_square.row*SQSIZE,SQSIZE,SQSIZE)
+            #blit
+            pygame.draw.rect(surface, color, rect,width=3)
+
+
     # other methods
     def next_turn(self):
         self.next_player = 'white' if self.next_player == 'black' else 'black'
+    
+    def set_hover(self, row,col):
+        self.hovered_square = self.board.squares[row][col]
--- src/main.py
+++ src/main.py
@@ -27,6 +27,8 @@ class Main:
             game.show_moves(screen)
             game.show_pieces(screen)
 
+            game.show_hover(screen)
+
             if dragger.dragging:
                 dragger.update_blit(screen)
 
@@ -51,6 +53,11 @@ class Main:
                             game.show_pieces(screen)
                 # mouse motion
                 elif event.type == pygame.MOUSEMOTION:
+                    motion_row = event.pos[1] // SQSIZE
+                    motion_col = event.pos[0] // SQSIZE
+                    
+                    game.set_hover(motion_row, motion_col)
+                    
                     if dragger.dragging:
                         dragger.update_mouse(event.pos)
                         # show methord
@@ -58,6 +65,7 @@ class Main:
                         game.show_last_move(screen)
                         game.show_moves(screen)
                         game.show_pieces(screen)
+                        game.show_hover(screen)
                         dragger.update_blit(screen)
                 # mouse release
                 elif event.type == pygame.MOUSEBUTTONUP:
